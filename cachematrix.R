6ce863401b1588c203689fc2ccaa9d495d534f3b
rohitalvs <rohitalvs94@gmail.com>
2017-05-29 01:46:02 +0530

    Assignment 2
    Complete functions with code written to test them as well.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function creates a special "matrix" object that can cache its inverse
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) 
+{
+  invert <- NULL
+  
+  set <- function(y)
+    {
+    x <<- y
+    invert <<- NULL
+  }
+  
+  get <- function() x
+  s_inv <- function(a) invert <<- a
+  g_inv <- function() invert
+  list(set = set, get = get, s_inv = s_inv, g_inv = g_inv)
 }
 
 
-## Write a short comment describing this function
+## Computes inverse of special matrix returned by makeCacheMatrix
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) 
+
+{
+ 
+  invert <- x$g_inv()
+  
+  if(!is.null(invert))
+    {
+      message("Retrieving the inverse from cache")
+      return(invert)
+  }
+  
+  data <- x$get()
+  invert <- solve(data)
+  x$s_inv(invert)
+  invert      
 }
+
+# Solution testing case 1. Run to see o/p as part of solution
+
+mat1<- makeCacheMatrix(matrix(2:5, 2, 2))
+mat1$get()
+mat1$g_inv()
+cacheSolve(mat1)
+cacheSolve(mat1)
+
+# Solution testing case 2
+
+mat1$set(matrix(c(1, 4, 2, 6), 2, 2))
+mat1$get()
+mat1$g_inv()
+cacheSolve(mat1)
+cacheSolve(mat1)
